311f3b9cf9bb2e5a5e09b57950800ac188e278ee
bomberest0 <bomberest0@gmail.com>
2017-05-09 12:03:48 +0300

    Sub

---
+++ Anvelop/Common/Loadable/CachedResources.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Anvelop.Common.Loadable
+{
+	public class CachedResources : MonoBehaviour
+	{
+		static readonly Dictionary<string, Object> Cache = new Dictionary<string, Object>();
+
+		public T Load<T>(string path) where T : Object
+		{
+			if (!Cache.ContainsKey(path))
+			{
+				Cache[path] = new LoadableResource<T>(path).Load();
+			}
+
+			return (T)Cache[path];
+		}
+	}
+}
